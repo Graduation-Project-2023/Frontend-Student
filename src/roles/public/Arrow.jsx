490afcfff932cf2d4f35ec8af75f2c9dfe3be03e
stevensamir20 <steven.samir@eng.suez.edu.eg>
2023-03-19 00:58:25 +0200

    structure

--- src/roles/public/Arrow.jsx
+++
@@ -1,9 +0,0 @@
-import "./Arrow.scss";
-export const Arrow = () => {
-    return (
-        <div className="arrow">
-            <div className="trapezoid-1"></div>
-            <div className="trapezoid-2"></div>
-             </div>
-    )
-}
