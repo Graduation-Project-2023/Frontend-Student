1e62a47fe885c57186d87e60e18acb54aacdb58d
MOoradwan1 <ugs.161570@eng.suez.edu.eg>
2023-03-19 16:59:42 +0200

    Translation + Resizing

--- src/roles/public/staff/Staff.jsx
+++ src/roles/public/staff/Staff.jsx
@@ -1,26 +1,22 @@
+import { useTranslation } from "react-i18next";
+import { StaffData } from "./StaffData.js";
+
 // Reusable Components and Images
 import { LandingNavbar } from "../../../components/header/LandingNavbar";
 import image6 from "../../../shared/images/image6.jpg";
 
 export const Staff = () => {
+  const { t } = useTranslation();
   return (
     <div>
       <LandingNavbar />
       <div className="staff-container-1">
         <div>
           <div>
-            <h3 className="staff-container-1-title">Induction</h3>
+            <h3 className="staff-container-1-title">{t('staff.container-1-title')}</h3>
           </div>
           <p className="staff-container-1-text">
-            The induction event/session is your start point in the process of
-            becoming familiar with, and settling into, University life. During
-            your induction session you will be introduced to your workplace,
-            team, and supervisors. you will be informed with your duties and
-            schedules. Also, you will be handed your ID card, username, and
-            passwords of the professors-related digital platforms. Your
-            induction session is your opportunity to investigate, to ask
-            questions, and to introduce yourself to the employees. Always feel
-            free to explore your environment, and ask for help if needed.
+          {t('staff.container-1-text')}
           </p>
         </div>
         <div>
@@ -30,65 +26,35 @@ export const Staff = () => {
 
       <div className="staff-container-2">
         <div className="staff-container-2-text">
-          Suez Canal University provides the academic staff with housing
-          services. The service is manifested in the tutors Lounge located in
-          El-Sheikh Zayed district next to the International Swimming pool in in
-          Ismailia. The lounge consists of 2 blocks that have a total of 45
-          rooms with a housing capacity of 180 residents. To qualify for
-          admission, applicants must complete all secondary school graduation
-          requirements, maintain a high level of academic performance and submit
-          all required documents before the set deadlines. Having the minimum
-          academic requirements does not guarantee admission to the University,
-          but rather ensures entry to the selection process. There is a minimum
-          level of academic achievement for each faculty that is decided by the
-          supreme council of universities each year.
+        {t('staff.container-2-text')}
         </div>
         <div className="staff-container-2-title">
-          Accommodation / Staff Housing
+        {t('staff.container-2-title')}
         </div>
       </div>
 
       <div className="staff-container-3">
-        <div className="staff-container-3-title">How To Apply</div>
+        <div className="staff-container-3-title">{t('staff.container-3-title')}</div>
         <div className="staff-container-3-text">
-          The Library aspires to be the main source of intellectual and
-          civilized knowledge, with its sources of information in science and
-          culture as well as its material and human capabilities and support
-          services in the advancement of knowledge and research movement and the
-          development of the educational process. Library services:
+          {t('staff.container-3-list-title')}
           <ul>
-            <li>Traditional information sources.</li>
-            <li>Traditional periodicals.</li>
-            <li>
-              Searching databases and information networks electronically.
-            </li>
-            <li>Electronic periodicals.</li>
-            <li>Relationships with local publishers.</li>
-            <li>Photography and reproduction.</li>
-            <li>Ongoing briefing.</li>
-            <li>Email and Internet.</li>
-            <li>Reference services.</li>
-            <li>Technical training for beneficiaries.</li>
-            <li>Guidance for students in using the library facilities.</li>
+          {StaffData.firstList.map((item) => (
+            <li key={item.id}>{t(item.title)}</li>
+          ))}
           </ul>
         </div>
       </div>
 
       <div className="staff-container-4">
         <div className="staff-container-4-text">
-          Available courses:
+        {t('staff.container-4-list-title')}
           <ul>
-            <li>Search Engines</li>
-            <li>Microsoft PowerPoint</li>
-            <li> Microsoft Word.</li>
-            <li>Search Excel</li>
-            <li>Endnote</li>
-            <li>Microsoft Excel</li>
-            <li>Web-Authorizing using Dream Weaver.</li>
-            <li> Plagiarism checker and avoidance</li>
+          {StaffData.secondList.map((item) => (
+            <li key={item.id}>{t(item.title)}</li>
+          ))}
           </ul>
         </div>
-        <div className="staff-container-4-title">Professional development</div>
+        <div className="staff-container-4-title">{t('staff.container-4-title')}</div>
       </div>
     </div>
   );
---
+++ src/roles/public/staff/StaffData.js
@@ -0,0 +1,25 @@
+export const StaffData = {
+    firstList: [
+      { id: 0, title: "libraryServices.1" },
+      { id: 1, title: "libraryServices.2" },
+      { id: 2, title: "libraryServices.3" },
+      { id: 3, title: "libraryServices.4" },
+      { id: 4, title: "libraryServices.5" },
+      { id: 5, title: "libraryServices.6" },
+      { id: 6, title: "libraryServices.7" },
+      { id: 7, title: "libraryServices.8" },
+      { id: 8, title: "libraryServices.9" },
+      { id: 9, title: "libraryServices.10" },
+    ],
+    secondList: [
+      { id: 0, title: "availableCourses.1" },
+      { id: 1, title: "availableCourses.2" },
+      { id: 2, title: "availableCourses.3" },
+      { id: 3, title: "availableCourses.4" },
+      { id: 4, title: "availableCourses.5" },
+      { id: 5, title: "availableCourses.6" },
+      { id: 6, title: "availableCourses.7" },
+      { id: 7, title: "availableCourses.8" },
+    ],
+  };
+  
