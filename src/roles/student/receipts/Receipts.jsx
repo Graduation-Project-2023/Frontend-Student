1253978bf6b287183578d8e84e5a206449159b68
DoniaAboelyazed <doniaaboelyazed12@gmail.com>
2023-05-24 23:00:33 +0300

    'receipts'

--- src/roles/student/receipts/Receipts.jsx
+++ src/roles/student/receipts/Receipts.jsx
@@ -1,57 +1,62 @@
 import { SidebarCont } from "../../../components/header/SidebarCont";
 import { BiPrinter } from "react-icons/bi";
 import { HiScissors } from "react-icons/hi";
+import { useTranslation } from "react-i18next";
 
 export const Receipts = () => {
+  const { t } = useTranslation();
   return (
     <SidebarCont>
       <table className="paymentTable">
         <tr>
-          <th className="paymentTable-header"> First semester 2022/2023</th>
+          <th className="paymentTable-header" colspan="2"> {t("receipts.semester")}</th>
           <th className="paymentTable-header"> </th>
         </tr>
         <tr>
-          <td className="paymentTable-content">Number of credit hours</td>
-          <td className="paymentTable-content">
-            19 <span className="paymentTable-content-price">Cr.hrs </span>
+          <td className="paymentTable-content">{t("receipts.creditHrs")}</td>
+          <td className="paymentTable-content-total">
+            19 <span className="paymentTable-content-price">{t("receipts.hrs")}  </span>
           </td>
         </tr>
         <tr>
-          <td className="paymentTable-content">Annual fees</td>
-          <td className="paymentTable-content">
-            615 <span className="paymentTable-content-price">EGP </span>
+          <td className="paymentTable-content">{t("receipts.annual")}</td>
+          <td className="paymentTable-content-total">
+            615 <span className="paymentTable-content-price">{t("receipts.EGP")} </span>
           </td>
         </tr>
         <tr>
           <td className="paymentTable-content">
-            Retake fees
-            <span className="paymentTable-content-ex">/50 per subject</span>
+          {t("receipts.retake")}
+            <span className="paymentTable-content-ex">/{t("receipts.per")}</span>
           </td>
-          <td className="paymentTable-content">
-            50 <span className="paymentTable-content-price">EGP </span>
-          </td>
-        </tr>
-        <tr>
-          <td className="paymentTable-content">Book fees</td>
-          <td className="paymentTable-content">
-            320 <span className="paymentTable-content-price">EGP </span>
+          <td className="paymentTable-content-total">
+            50 <span className="paymentTable-content-price">{t("receipts.EGP")}  </span>
           </td>
         </tr>
         <tr>
-          <td className="paymentTable-content">Total fees</td>
-          <td className="paymentTable-content">
-            1000 <span className="paymentTable-content-price">EGP </span>
+          <td className="paymentTable-content">{t("receipts.book")}</td>
+          <td className="paymentTable-content-total">
+            320 <span className="paymentTable-content-price">{t("receipts.EGP")}  </span>
+          </td>
+        </tr>
+        <tr>
+          <td className="paymentTable-content">{t("receipts.totalFees")}</td>
+          <td className="paymentTable-content-total">
+            1000 <span className="paymentTable-content-price">{t("receipts.EGP")}  </span>
           </td>
         </tr>
       </table>
-      <button className="paymentBtn">
-        Print <BiPrinter />
+      <div className="receiptsBtn">
+      <button className="receiptsBtn-design">
+      {t("receipts.print")} <BiPrinter />
       </button>
-      <div>
-        <span className="paymentScissor">
+      </div>
+      <div className="dashedScissor">
+        <div className="Scissor">
           <HiScissors />
-        </span>
-        <span className="paymentLine"> </span>
+        </div>
+        <div className="dashedLine"> 
+        </div>
       </div>
     </SidebarCont>
   );
