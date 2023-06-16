00528d59b0d47523414fc7dd2438191a5b89e052
hunbunbuns <UGS.161477@eng.suez.edu.eg>
2023-06-16 22:23:46 +0300

    landing page

--- src/roles/public/home/Home.jsx
+++ src/roles/public/home/Home.jsx
@@ -1,141 +1,92 @@
 import { useTranslation } from "react-i18next";
 
 // Reusable Components and Images
-import managInfo from "../../../shared/images/managmentInfo.jpeg";
-import HR from "../../../shared/images/humanResource.jpg";
-import eLearning from "../../../shared/images/eLearning.jpeg";
+import right from "./right.png";
+import left from "./left.png";
+import bck from "./bck.png";
+import managInfo from "../../../shared/images/managmentInfo.png";
+import HR from "../../../shared/images/humanResource.png";
+import eLearning from "../../../shared/images/eLearning.png";
 import digital from "../../../shared/images/digitalTrans.png";
-import graduate from "../../../shared/images/graduating.jpeg";
-import { HiArrowNarrowRight } from "react-icons/hi";
+import corona from "../../../shared/images/corona.png";
+import grad from "../../../shared/images/grad.png";
+import meeting from "../../../shared/images/meeting.png";
+import { FaArrowCircleRight, FaArrowCircleLeft} from "react-icons/fa";
 
 export const Home = () => {
-  const { t } = useTranslation();
+  const { t, i18n } = useTranslation();
+
+  let arabimg = {};
+  let engimg = { transform: "rotateY(180deg)" };
 
   return (
     <>
       <div className="home">
         <div className="home-bg">
-          {/*<div className="home-btns">
-            <button className="home-btn home-btn-side">
-              <h1 className="home-btn-icon">
-                <FaChalkboardTeacher />
-              </h1>
-              {t("home.staff")}
-            </button>
-            <button className="home-btn">
-              <h1 className="home-btn-icon">
-                <FaChild />
-              </h1>
-              {t("home.undergraduate")}
-            </button>
-            <button className="home-btn  home-btn-side">
-              <h1 className="home-btn-icon">
-                <HiOutlineAcademicCap />
-              </h1>
-              {t("home.postgraduate")}
-            </button>
-  </div>*/}
-        </div>
-      </div>
-
-      <div className="aboutUs">
-        <h4 className="aboutUs-title"> {t("home.aboutUs")}</h4>
-        <p className="aboutUs-text">{t("home.aboutUsDescription")}</p>
-
-        <h5 className="aboutUs-end">{t("home.contactUs")}</h5>
-      </div>
-
-      <div className="home-container">
-        <div className="home-container-title">
-          <h3 className="home-container-title-name"> {t("home.training")}</h3>
-          <h3 className="home-container-title-name"> {t("home.services")}</h3>
-        </div>
-        <div className="home-container-grid">
-          <div className="home-container-grid-news">
-            <img
-              className="home-container-grid-news-pic"
-              src={managInfo}
-              alt="/"
-            />
-
-            <h4 className="home-container-grid-news-title">
-              {t("home.managementUnit")}
-            </h4>
-            <h6 className="home-container-grid-news-content">
-              {t("home.managementUnitNews")}
-            </h6>
-            <button className="home-container-grid-news-btn">
-              {t("home.more")}
-            </button>
-          </div>
-
-          <div className="home-container-grid-news">
-            <img className="home-container-grid-news-pic" src={HR} alt="/" />
-            <h4 className="home-container-grid-news-title">{t("home.HR")}</h4>
-            <h6 className="home-container-grid-news-content">
-              {t("home.HRNews")}
-            </h6>
-            <button className="home-container-grid-news-btn">
-              {t("home.more")}
-            </button>
-          </div>
-
-          <div className="home-container-grid-news">
-            <img
-              className="home-container-grid-news-pic"
-              src={eLearning}
-              alt="/"
-            />
-            <h4 className="home-container-grid-news-title">
-              {" "}
-              {t("home.eLearning")}
-            </h4>
-            <h6 className="home-container-grid-news-content">
-              {t("home.eLearningNews")}
-            </h6>
-            <button className="home-container-grid-news-btn">
-              {t("home.more")}
-            </button>
-          </div>
-          <div className="home-container-grid-news">
-            <img
-              className="home-container-grid-news-pic"
-              src={digital}
-              alt="/"
-            />
-            <h4 className="home-container-grid-news-title">
-              {t("home.digitalTrans")}
-            </h4>
-            <h6 className="home-container-grid-news-content">
-              {t("home.managementUnitNews")}
-            </h6>
-
-            <button className="home-container-grid-news-btn">
-              {t("home.more")}
-            </button>
-          </div>
-        </div>
-      </div>
-      <div className="latestNews">
-        <div className="latestNews-title">
-          {t("home.latestNews")} <br /> {t("home.events")}
-        </div>
-
-        <div className="latestNews-container">
-          <img className="latestNews-container-pic" src={graduate} alt="/" />
-          <h6 className="latestNews-container-date"> {t("home.newsDates")}</h6>
-          <h6 className="latestNews-container-news">{t("home.news")}</h6>
-          <div className="latestNews-container-btn">
-            <HiArrowNarrowRight />
+          <div className="home-bg-img">
+            <img src={right} alt="/" style={i18n.language === "ar" ? arabimg : engimg}/>
+            <img src={left} alt="/" style={i18n.language === "ar" ? arabimg : engimg}/>
           </div>
         </div>
 
-        <div className="latestNews-container">
-          <img className="latestNews-container-pic" src={graduate} alt="/" />
-          <h6 className="latestNews-container-date">{t("home.newsDates")}</h6>
-          <h6 className="latestNews-container-news">{t("home.news")}</h6>
-          <div className="latestNews-container-btn">
-            <HiArrowNarrowRight />
+        <div className="home-aboutUs" style={{ backgroundImage: `url(${bck})` }}>
+          <h4 className="home-aboutUs-title"> {t("home.aboutUs")}</h4>
+          <p className="home-aboutUs-text">{t("home.aboutUsDescription")}</p>
+          <div className="home-aboutUs-end">{t("home.contactUs")}</div>
+        </div>
+
+
+        <div className="home-container">
+          <div className="home-container-title">
+            <h3 className="home-container-title-name"> {t("home.training")}</h3>
+          </div>
+          <div className="home-container-grid">
+            <div className="home-container-grid-news">
+              <div className="home-container-grid-news-pic"><img src={managInfo} alt="/"/></div>
+              <br />
+              <h6 className="home-container-grid-news-title">{t("home.managementUnit")}</h6>
+              {/* <button className="home-container-grid-news-btn">{t("home.more")}</button> */}
+            </div>
+            <div className="home-container-grid-news">
+              <div className="home-container-grid-news-pic"><img src={HR} alt="/"/></div>
+              <br />
+              <h6 className="home-container-grid-news-title">{t("home.HR")}</h6>
+              {/* <button className="home-container-grid-news-btn">{t("home.more")}</button> */}
+            </div>
+            <div className="home-container-grid-news">
+              <div className="home-container-grid-news-pic"><img src={eLearning} alt="/"/></div>
+              <br />
+              <h6 className="home-container-grid-news-title">{t("home.eLearning")}</h6>
+              {/* <button className="home-container-grid-news-btn">{t("home.more")}</button> */}
+            </div>
+            <div className="home-container-grid-news">
+              <div className="home-container-grid-news-pic"><img src={digital} alt="/" /></div>
+              <br />
+              <h6 className="home-container-grid-news-title">{t("home.digitalTrans")}</h6>
+              {/* <button className="home-container-grid-news-btn">{t("home.more")}</button> */}
+            </div>
+          </div>
+        </div>
+
+        <div className="home-latestNews">
+          <div className="home-latestNews-title">
+            <h3>{t("home.latestNews")}{t("home.events")}</h3>
+          </div>
+          <div className="home-latestNews-grid">
+            <div className="home-latestNews-grid-icon" style={i18n.language === "ar" ? arabimg : engimg}><FaArrowCircleRight></FaArrowCircleRight></div>
+            <div className="home-latestNews-grid-news">
+              <div className="home-latestNews-grid-news-pic"><img src={corona} alt="/" /></div>
+              <h6 className="home-latestNews-grid-news-title">{t("home.news")}</h6>
+            </div>
+            <div className="home-latestNews-grid-news">
+              <div className="home-latestNews-grid-news-pic"><img src={meeting} alt="/" /></div>
+              <h6 className="home-latestNews-grid-news-title">{t("home.news")}</h6>
+            </div>
+            <div className="home-latestNews-grid-news">
+              <div className="home-latestNews-grid-news-pic"><img src={grad} alt="/" /></div>
+              <h6 className="home-latestNews-grid-news-title">{t("home.news")}</h6>
+            </div>
+            <div className="home-latestNews-grid-icon" style={i18n.language === "ar" ? arabimg : engimg}><FaArrowCircleLeft></FaArrowCircleLeft></div>
           </div>
         </div>
       </div>
