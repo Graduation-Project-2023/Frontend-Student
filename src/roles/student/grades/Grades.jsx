c31feb389ba40fae7328400c76952ea1fb4e0a31
stevensamir20 <steven.samir@eng.suez.edu.eg>
2023-07-05 19:29:16 +0300

    grades (ui/ux)

--- src/roles/student/grades/Grades.jsx
+++ src/roles/student/grades/Grades.jsx
@@ -1,4 +1,91 @@
+import { useState, useEffect } from "react";
+import { useAuth } from "../../../hooks/useAuth";
+import { useTranslation } from "react-i18next";
+import axios from "axios";
+import { STUDENT_URL } from "../../../shared/API";
+
+// Components
 import { SidebarCont } from "../../../components/header/SidebarCont";
+import { Backdrop } from "../../../components/loaders/Backdrop";
+import { CommonTable } from "../../../components/table/common/CommonTable";
+import { GradesTableHeadings, GPAData } from "./GradesTableHeadings";
+
 export const Grades = () => {
-  return <SidebarCont></SidebarCont>;
+  const [courses, setCourses] = useState([]);
+  const [userUX, setUserUX] = useState({
+    loading: true,
+    error: false,
+    errorMsg: "",
+  });
+  const authContext = useAuth();
+  const { t } = useTranslation();
+
+  const calculateGPA = (item) => {
+    const totalGrade =
+      +item.classworkScore ||
+      0 + +item.finalScore ||
+      0 + +item.idtermScore ||
+      0;
+    let gpaRange = null;
+    for (let i = 0; i < GPAData.length; i++) {
+      const range = GPAData[i];
+      if (totalGrade === 100) {
+        gpaRange = GPAData[0];
+        break;
+      } else if (
+        range.gpaFrom <= (4 * totalGrade) / 100 &&
+        range.gpaTo > (4 * totalGrade) / 100
+      ) {
+        gpaRange = range;
+        break;
+      }
+    }
+    return {
+      grade: gpaRange.grade,
+      gpa: (4 * totalGrade) / 100,
+    };
+  };
+
+  useEffect(() => {
+    setUserUX({ loading: true, error: false, errorMsg: "" });
+    axios
+      .get(STUDENT_URL + `/courses?studentId=${authContext.id}`)
+      .then((res) => {
+        console.log(res);
+        setCourses(
+          res.data.map((item) => ({
+            ...item.instance,
+            ...item,
+            total: +item.midtermScore + +item.classworkScore + +item.finalScore,
+            grade: calculateGPA(item).grade,
+            gpa: calculateGPA(item).gpa,
+          }))
+        );
+        setUserUX({
+          loading: false,
+          error: res.data.length === 0 ? true : false,
+          errorMsg: res.data.length === 0 ? "error.noGrades" : "",
+        });
+      })
+      .catch((err) => {
+        console.log(err);
+        setUserUX({ loading: false, error: true, errorMsg: "error.common" });
+      });
+    // eslint-disable-next-line
+  }, []);
+
+  return userUX.loading ? (
+    <Backdrop />
+  ) : (
+    <SidebarCont>
+      <div className="mb-5">
+        <CommonTable
+          header={t("receipts.semester")}
+          headings={GradesTableHeadings}
+          data={courses}
+          userUX={userUX}
+        />
+      </div>
+    </SidebarCont>
+  );
 };
---
+++ src/roles/student/grades/GradesTableHeadings.js
@@ -0,0 +1,29 @@
+export const GradesTableHeadings = [
+  {
+    id: 0,
+    label: "courses.name",
+    name: "name",
+  },
+  { id: 1, label: "courses.code", name: "code" },
+  { id: 2, label: "grades.classWork", name: "classworkScore" },
+  { id: 3, label: "grades.midterm", name: "midtermScore" },
+  { id: 4, label: "grades.final", name: "finalScore" },
+  { id: 5, label: "grades.total", name: "total" },
+  { id: 7, label: "grades.gpa", name: "gpa" },
+  { id: 6, label: "grades.grade", name: "grade" },
+];
+
+export const GPAData = [
+  { id: 1, grade: "A", gpaFrom: 3.7, gpaTo: 4 },
+  { id: 2, grade: "A-", gpaFrom: 3.3, gpaTo: 3.7 },
+  { id: 3, grade: "B+", gpaFrom: 3, gpaTo: 3.3 },
+  { id: 4, grade: "B", gpaFrom: 2.7, gpaTo: 3 },
+  { id: 5, grade: "B-", gpaFrom: 2.3, gpaTo: 2.7 },
+  { id: 6, grade: "C+", gpaFrom: 2, gpaTo: 2.3 },
+  { id: 7, grade: "C", gpaFrom: 1.7, gpaTo: 2 },
+  { id: 8, grade: "C-", gpaFrom: 1.3, gpaTo: 1.7 },
+  { id: 9, grade: "D+", gpaFrom: 1, gpaTo: 1.3 },
+  { id: 10, grade: "D", gpaFrom: 0.7, gpaTo: 1 },
+  { id: 11, grade: "D-", gpaFrom: 0.3, gpaTo: 0.7 },
+  { id: 12, grade: "F", gpaFrom: 0, gpaTo: 0.3 },
+];
